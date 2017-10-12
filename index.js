d6d00ddb1eba37bf356063632d9814ae54c0d9e3
hugo <hcgar00@gmail.com>
2017-10-12 16:05:32 +0300

    Get compact data - ongoing

--- index.js
+++ index.js
@@ -14,20 +14,44 @@ const log = (...args) => {
     console.log('');
     args.forEach(arg => console.log(arg));
     console.log('');
+};
+
+const logResponseData = (data) => {
+    const {
+        movies,
+        nextStart
+    } = data;
+    log(`RECEIVED ${movies.length} MOVIES, from "${movies[0].name}" to "${movies[movies.length - 1].name}", NEXT START: ${nextStart}`);
 }
 
 const getUrl = (listId, view, start) => {
     return `http://www.imdb.com/list/${listId}/?start=${start}&view=${view}&sort=created:desc`
 };
 
+const nextStartConfig = {
+    selector: '.see-more > .pages > .pagination > a',
+    // attr: 'href',
+    how: elem => {
+        if (elem.text().includes('Next'))  {
+            const href = elem.attr('href');
+            if (href && href[0] === '?') {
+                const start = querystring.parse(href.substr(1)).start;
+                return start ? parseInt(start) : undefined;
+            }
+        }
+        return undefined;
+    }
+};
+
 const getDetailedData = async(listId, start = 1) => {
     const url = getUrl(listId, VIEW_DETAIL, start);
     log('REQUESTING URL', url);
 
-    // const html = await fs.readFile('./imdb.html', 'UTF-8');
+    // const html = await fs.readFile('./imdb_detailed1.html', 'UTF-8');
     // const $ = cheerio.load(html);
+    // const data = await scrape.scrapeHTML($, {
 
-    const data = await scrape.scrapeHTML(url, {
+    const data = await scrape(url, {
         movies: {
             listItem: '.list_item > .info',
             data: {
@@ -43,41 +67,64 @@ const getDetailedData = async(listId, start = 1) => {
                 }
             }
         },
-        nextStart: {
-            selector: '.see-more > .pages > .pagination > a',
-            attr: 'href',
-            convert: v => {
-                if (v && v[0] === '?') {
-                    v = v.substr(1);
-                }
-                const start = querystring.parse(v).start;
-                return start ? parseInt(start) : undefined;
-            }
-        }
+        nextStart: nextStartConfig
     });
 
-    return data;
+    // logResponseData(data);
+
+    const movies = {};
+    data.movies.forEach(movie => movies[movie.name] = movie);
+    return {
+        movies,
+        nextStart: data.nextStart
+    };
 };
 
 const getCompactData = async(listId, start = 1) => {
     const url = getUrl(listId, VIEW_COMPACT, start);
+    log('REQUESTING URL', url);
+
+    const html = await fs.readFile('./imdb_compact1.html', 'UTF-8');
+    const $ = cheerio.load(html);
+    const data = await scrape.scrapeHTML($, {
+
+    // const data = await scrape(url, {
+        movies: {
+            listItem: '.list_item',
+            data: {
+                name: '.title > a',
+                created: 'td.created'
+            }
+        },
+        nextStart: nextStartConfig
+    });
+
+    log(data);
+    process.exit();
 
 };
 
 const run = async() => {
-    let { movies, nextStart } = await getDetailedData(listId);
-    log('RECEIVED DATA', movies, nextStart);
-    // while(nextStart) {
-    //     const data = await getDetailedData(listId, nextStart);
-    //     movies = movies.concat(data.movies);
-    //     nextStart = data.nextStart;
+    const listId = process.argv[2];
+    if (!listId) {
+        throw new Error('Invalid or missing listId');
+    }
 
-    //     log('RECEIVED DATA', movies, nextStart);
+    // let {movies,nextStart} = await getDetailedData(listId);
+
+    // while (nextStart) {
+    //     const data = await getDetailedData(listId, nextStart);
+    //     movies = Object.assign({}, movies, data.movies);
+    //     nextStart = data.nextStart;
     // }
 
-    // log('FINISHED', movies);
-    
-    // console.log($('.list_item > .info').first().find('div.secondary:first-of-type').length);
+    let {compactMovies,nextStart} = await getCompactData(listId);
+    console.log(compactMovies);
+
+
+
+
+    // log('FINISHED', movies, `Total movies ${Object.keys(movies).length}`);
 };
 
 run();
